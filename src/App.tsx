29be126c5d2ad80deb2e686be96628f0fe857261
Arashjp8 <arashjafarpour723@gmail.com>
2023-07-15 15:12:29 +0330

    tailwindcss not working problem solved

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import styles from "./style";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      App
-    </>
-  )
+    <div className="bg-primary w-full overflow-hidden">
+      <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+        <div className={`${styles.boxWidth}`}>Navbar</div>
+      </div>
+
+      <div className={`bg-primary ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>Hero</div>
+      </div>
+
+      <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>
+          <br />
+          Stats
+          <br />
+          Business
+          <br />
+          Billing
+          <br />
+          CardDeal
+          <br />
+          Testimonials
+          <br />
+          Clients
+          <br />
+          CTA
+          <br />
+          Footer
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
