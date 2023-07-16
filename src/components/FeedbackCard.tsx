d45dba8d122f0ac95db4491af87daa90090e5cc9
Arashjp8 <arashjafarpour723@gmail.com>
2023-07-16 15:13:35 +0330

    testimonials section added

--- src/components/FeedbackCard.tsx
+++ src/components/FeedbackCard.tsx
@@ -1,5 +1,46 @@
-const FeedbackCard = () => {
-  return <div>feedbackcard</div>;
+import { quotes } from "../assets";
+import styles from "../style";
+
+interface Props {
+  fb: { id: string; content: string; name: string; title: string; img: string };
+}
+
+const FeedbackCard = ({ fb }: Props) => {
+  return (
+    <div
+      className={`w-[370px] h-[395px] flex flex-col flex-wrap items-start hover:bg-dimWhite feature-card rounded-xl`}
+    >
+      <div className={`${styles.paddingY} pl-9`}>
+        <div>
+          <img
+            src={quotes}
+            alt="quotes"
+            className="w-[43px] h-[28px] object-contain"
+          />
+        </div>
+        <div>
+          <p
+            className={`${styles.paragraph} font-semibold max-w-[300px] mt-10`}
+          >
+            {fb.content}
+          </p>
+        </div>
+        <div className="mt-10 flex flex-row">
+          <img src={fb.img} alt="people01" className="w-[48px] h-[48px]" />
+          <span className="ml-5">
+            <h4
+              className={`font-poppins font-semibold  text-white xs:leading-[32px] leading-[20px] w-full sm:text-[26px] text-[20px]`}
+            >
+              {fb.name}
+            </h4>
+            <p className="font-poppins font-normal text-dimWhite text-[16px]">
+              {fb.title}
+            </p>
+          </span>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default FeedbackCard;
--- src/components/Testimonials.tsx
+++ src/components/Testimonials.tsx
@@ -1,5 +1,28 @@
+import { feedback } from "../constants";
+import styles, { layout } from "../style";
+import FeedbackCard from "./FeedbackCard";
+
 const Testimonials = () => {
-  return <div>testimonials</div>;
+  return (
+    <section className={layout.section}>
+      <div className="flex flex-col flex-1">
+        <div className={`flex-1 flex-row ${styles.flexStart}`}>
+          <h2 className={styles.heading2}>
+            What people are <br className="sm:block hidden" /> saying about us
+          </h2>
+          <p className={`${styles.paragraph} max-w-[450px] mt-10`}>
+            Everything you need to accept card payments and grow your business
+            anywhere on the planet.
+          </p>
+        </div>
+        <div className={`flex flex-row justify-between items-center mt-10`}>
+          {feedback.map((fb) => (
+            <FeedbackCard key={fb.id} fb={fb} />
+          ))}
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Testimonials;
