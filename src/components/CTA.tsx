0bdf31210d751335584eceddce91bea1695656aa
Arashjp8 <arashjafarpour723@gmail.com>
2023-07-16 19:06:21 +0330

    CTA section updated

--- src/components/CTA.tsx
+++ src/components/CTA.tsx
@@ -4,19 +4,17 @@ import Button from "./Button";
 const CTA = () => {
   return (
     <section
-      className={`${layout.section} feature-card rounded-2xl px-20 mt-20`}
+      className={`${styles.flexCenter} ${styles.marginY} ${styles.padding} sm:flex-row flex-col bg-black-gradient-2 rounded-[20px] box-shadow`}
     >
-      <div
-        className={`flex-1 ${styles.flexStart} md:flex-row flex-col justify-between`}
-      >
-        <span>
-          <h2 className={styles.heading2}>Let’s try our service now!</h2>
-          <p className={`${styles.paragraph} max-w-[460px]`}>
-            Everything you need to accept card payments and grow your business
-            anywhere on the planet.
-          </p>
-        </span>
-        <Button styles="mt-10" />
+      <div className={`flex-1 flex flex-col`}>
+        <h2 className={styles.heading2}>Let’s try our service now!</h2>
+        <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+          Everything you need to accept card payments and grow your business
+          anywhere on the planet.
+        </p>
+      </div>
+      <div className={`${styles.flexCenter} sm:ml-10 ml-0 sm:mt-0 mt-10`}>
+        <Button styles="" />
       </div>
     </section>
   );
